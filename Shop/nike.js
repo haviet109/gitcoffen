2d6eb17204acbe0f249efe35bdbb070f88043127
Việt Nguyễn <nguyenhaviet1904211@gmail.com>
2023-07-21 19:36:40 +0700

    push

---
+++ Shop/nike.js
@@ -0,0 +1,17 @@
+let search = document.querySelector('.search-box');
+document.querySelector('#search-icon').onclick = () => {
+    search.classList.toggle('active');
+    console.log('active');
+}
+
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-icon').onclick = () => {
+    navbar.classList.toggle('active');
+  
+}
+ 
+
+let header = document.querySelector('header');
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow',window.scrollY >0);
+});
